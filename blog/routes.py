490148aa9192c7df02697c6300d3b8e1b54c8289
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-09-14 21:16:02 +0200

    Added /about endpoint. TODO

--- blog/routes.py
+++ blog/routes.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def homepage():
     return render_template("base.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")

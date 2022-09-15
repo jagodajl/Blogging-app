82e6a93b6840640af788f81f9f83e853a7c3ba5a
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-09-15 15:17:08 +0200

    models.py

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,4 +1,19 @@
 from routes import app
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+# from blog import routes, models
+
+# app.config.from_object(Config)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+# @app.shell_context_processor
+# def make_shell_context():
+#   return {
+#       "db": db,
+#       "Entry": models.Entry
+#   }
 
 if __name__ == '__main__':
     app.run(debug=True)
--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,11 @@
-# models
+from . import db
+import datetime
+
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False,
+                         default=datetime.datetime.utcnow)
+    is_published = db.Column(db.Boolean, default=False)

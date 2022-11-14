3a4dff9b05867041fa062781475b6ceac4871d1e
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-11-14 16:42:03 +0100

    Ustawienie na sztywno loginu i hasła

--- config.py
+++ config.py
@@ -5,10 +5,9 @@ class Config(object):
     DEVELOPMENT = os.environ.get("DEV_SET")
     FLASK_HTPASSWD_PATH = "/secret/.htpasswd"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///data_blog.db"
-    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "")
-    # ADMIN_USERNAME = "admin"
-    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "")
-    # ADMIN_PASSWORD = "admin"
-
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///data_blog.db"
+    # ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "")
+    ADMIN_USERNAME = "admin"
+    # ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "")
+    ADMIN_PASSWORD = "admin"

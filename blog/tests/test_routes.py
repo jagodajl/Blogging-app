af8717164ee0f551b400e3aba521614058e88630
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-11-21 11:27:07 +0100

    Code testing part 3

--- blog/tests/test_routes.py
+++ blog/tests/test_routes.py
@@ -18,18 +18,22 @@ def today_date():
 
 def test_home_page(app_client):
     with app_client:
+        expected_html_cut_off = "Life is short, and the world is wide..."
         response_home = app_client.get("/")
         assert response_home.status_code == 200
-        # TODO:
-        #   add more assertions
+        html = str(response_home.get_data())
+        count = html.count(expected_html_cut_off)
+        assert count == 1
 
 
 def test_open_new_login_form(app_client):
     with app_client:
+        expected_html_cut_off = "Login Page"
         response_get_login_form = app_client.get("/login/")
         assert response_get_login_form.status_code == 200
-        # TODO:
-        #   add more assertions
+        html = str(response_get_login_form.get_data())
+        count = html.count(expected_html_cut_off)
+        assert count == 1
 
 
 def test_login(app_client):
@@ -40,24 +44,23 @@ def test_login(app_client):
         }
         response_login = app_client.post("/login/", data=form_data, follow_redirects=True)
         assert response_login.status_code == 200
-        # TODO:
-        #   add more assertions
 
 
 def test_logout(app_client):
     with app_client:
         response_logout = app_client.post("/logout/", follow_redirects=True)
         assert response_logout.status_code == 200
-        # TODO:
-        #   add more assertions
 
 
 def test_open_new_post_form(app_client):
     with app_client:
+        expected_html_cut_off = "Add new post"
         response_get_post_form = app_client.get("/post/")
         assert response_get_post_form.status_code == 200
+        html = str(response_get_post_form.get_data())
+        count = html.count(expected_html_cut_off)
+        assert count == 1
         # TODO:
-        #   add more assertions
         #   mocking @login_required
 
 
@@ -71,19 +74,24 @@ def test_create_new_post(app_client):
         response_create_post = app_client.post("/post/", data=form_data, follow_redirects=True)
         assert response_create_post.status_code == 200
         # TODO:
-        #   add more assertions
         #   mocking @login_required
 
 
 def test_open_edit_post_form(app_client, monkeypatch):
     with app_client:
-        edit_id = 3
-        # mocked_post_entry = Entry(id=1, title="Title", body="Body", post_img="Mysz", pub_date=today_date)
+        existing_edit_id = 7
+        expected_html_cut_off = "Add new post"
+        expected_img_utl_for_edit = "https://www.miavolta.com/wp-content/uploads/2022/02/Pula-6.jpg"
+        # mocked_post_entry = Entry(id=1, title="Title", body="Body", post_img="post_img", pub_date=today_date)
         # monkeypatch.setattr(service, 'get_entry_by_id', lambda mock: mocked_post_entry)
-        response_get_form_edit_post = app_client.get(f"/edit-post/{edit_id}", follow_redirects=False)
+        response_get_form_edit_post = app_client.get(f"/edit-post/{existing_edit_id}", follow_redirects=False)
         assert response_get_form_edit_post.status_code == 200
+        html = str(response_get_form_edit_post.get_data())
+        count = html.count(expected_html_cut_off)
+        count_img_url = html.count(expected_img_utl_for_edit)
+        assert count == 1
+        assert count_img_url == 1
         # TODO:
-        #   add more assertions - Mysz
         #   mocking @login_required
 
 
@@ -95,53 +103,58 @@ def test_edit_post(app_client, monkeypatch):
         response_get_form_edit_post = app_client.post(f"/edit-post/{edit_id}", follow_redirects=True)
         assert response_get_form_edit_post.status_code == 200
         # TODO:
-        #   add more assertions
         #   mocking @login_required
 
 
-def test_open_show_drafts_form(app_client, monkeypatch):
+def test_open_show_drafts_form(app_client):
     with app_client:
+        expected_html_cut_off = "My Drafts"
         response_get_form_show_drafts = app_client.get("/drafts/")
         assert response_get_form_show_drafts.status_code == 200
-        # TODO:
-        #   add more assertions
+        html = str(response_get_form_show_drafts.get_data())
+        count = html.count(expected_html_cut_off)
+        assert count == 1
 
 
-def test_delete_draft_non_existing(app_client, monkeypatch):
+def test_delete_draft_non_existing(app_client):
     with app_client:
-        delete_non_existing_id = 1
+        delete_non_existing_id = -1
         response_get_form_show_drafts = app_client.get(f"/delete/{delete_non_existing_id}")
         assert response_get_form_show_drafts.status_code == 404
 
 
-def test_delete_draft_existing(app_client, monkeypatch):
+def test_delete_draft_existing(app_client):
     with app_client:
         delete_existing_id = 8
         response_get_form_show_drafts = app_client.get(f"/delete/{delete_existing_id}", follow_redirects=True)
         assert response_get_form_show_drafts.status_code == 200
-        # TODO:
-        #   add more assertions
 
 
 def test_open_contact_form(app_client):
     with app_client:
+        expected_html_cut_off = "Get in Touch"
         response_get_login_form = app_client.get("/contact/")
         assert response_get_login_form.status_code == 200
-        # TODO:
-        #   add more assertions
+        html = str(response_get_login_form.get_data())
+        count = html.count(expected_html_cut_off)
+        assert count == 1
 
 
 def test_contact(app_client):
     with app_client:
+        expected_html_cut_off = "Your email has been sent!"
         response_get_login_form = app_client.post("/contact/", follow_redirects=True)
         assert response_get_login_form.status_code == 200
-        # TODO:
-        #   add more assertions
+        html = str(response_get_login_form.get_data())
+        count = html.count(expected_html_cut_off)
+        assert count == 1
 
 
 def test_about_page(app_client):
     with app_client:
+        expected_html_cut_off = "HELLO FELLOW TRAVELERS!"
         response_about = app_client.get("/about/")
         assert response_about.status_code == 200
-        # TODO:
-        #   add more assertions
+        html = str(response_about.get_data())
+        count = html.count(expected_html_cut_off)
+        assert count == 1
